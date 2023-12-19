99885bb6da8c2ea1c83cee8ab7705a1399828661
malikbreeze <abdulmalikidris680@gmail.com>
2023-12-19 10:50:34 -0800

    5-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,7 +16,6 @@ class Square:
         @property
         def size(self):
             """Get/set the current size of the square."""
-            return (self.__size)
 
         @size.setter
         def size(self, value):
